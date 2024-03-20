89f370ba5c8892056539ef19640539516d0bb4cf
adi99304 <140711660+adi99304@users.noreply.github.com>
2024-03-21 00:07:03 +0530

    firebase installed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:project/pages/CategoryDetailPage.dart';
@@ -8,8 +7,14 @@ import 'package:project/pages/LoginPage.dart';
 import 'package:project/pages/MoviePage.dart';
 import 'package:project/pages/ProfilePage.dart';
 import 'package:project/pages/UserPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -22,7 +27,6 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-
     super.initState();
   }
 
@@ -33,85 +37,8 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         scaffoldBackgroundColor: Color(0xFF0F111D),
       ),
-      home: AnimatedSplashScreen(
-          splash: Center(
-              child: ClipRRect(
-            borderRadius: BorderRadius.circular(40),
-            child: Container(
-              width: 300,
-              height: 300,
-              color: Colors.amber,
-              child: Center(
-                child: Text(
-                  "IMDb",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 85,
-                    fontWeight: FontWeight.w900,
-                  ),
-                ),
-              ),
-            ),
-          )),
-          duration: 2500,
-          splashTransition: SplashTransition.scaleTransition,
-          backgroundColor: Colors.black,
-          //   Center(
-          //   child: Column(
-          //     mainAxisAlignment: MainAxisAlignment.center,
-          //     children: [
-          //       Container(
-          //         height: 100,
-          //         width: 100,
-          //         color: Colors.blue,
-          //       ),
-          //       Container(
-          //         child: Text(
-          //           '2 sec ruko bhai',
-          //           style: TextStyle(color: Colors.lightBlue, fontSize: 24, fontWeight: FontWeight.bold),
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          nextScreen: LoginPage(
-            title: 'LOGIN',
-          )),
+      home: LoginPage(title: 'LOGIN'),
       routes: {
-        // "/": (context) => AnimatedSplashScreen(
-        //     splash: Text(
-        //       "IMDb",
-        //       style: TextStyle(
-        //         color: Colors.amber,
-        //         fontSize: 70,
-        //         fontWeight: FontWeight.w500,
-        //       ),
-        //     ),
-        //     duration: 2000,
-        //     splashTransition: SplashTransition.fadeTransition,
-        //     backgroundColor: Colors.black,
-        //     //   Center(
-        //     //   child: Column(
-        //     //     mainAxisAlignment: MainAxisAlignment.center,
-        //     //     children: [
-        //     //       Container(
-        //     //         height: 100,
-        //     //         width: 100,
-        //     //         color: Colors.blue,
-        //     //       ),
-        //     //       Container(
-        //     //         child: Text(
-        //     //           '2 sec ruko bhai',
-        //     //           style: TextStyle(color: Colors.lightBlue, fontSize: 24, fontWeight: FontWeight.bold),
-        //     //         ),
-        //     //       ),
-        //     //     ],
-        //     //   ),
-        //     // ),
-        //     nextScreen: LoginPage(
-        //       title: 'FLUTTER LOGIN',
-        //     )),
-        // "animatedsplashscreen": (context) => MyHomePage(title: 'Flutter Login Demo'),
         "loginpage": (context) => LoginPage(title: "Flutter Login"),
         "homepage": (context) => HomePage(),
         "categoryPage": (context) => CategoryPage(),
