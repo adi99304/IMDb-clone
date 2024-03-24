cbc77cb19b038bb09fbc6c0b699b7f392850777e
Chaitanya Kanade <chaitanyakanade2405@gmail.com>
2024-03-24 13:49:24 +0530

    modify

--- lib/pages/RomanceDetailPage.dart
+++ lib/pages/RomanceDetailPage.dart
@@ -1,17 +1,231 @@
 import 'package:flutter/material.dart';
 
-class RomanceDetailPage extends StatelessWidget {
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
   @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Romance Genre',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        brightness: Brightness.dark,
+      ),
+      home: RomanceDetailPage(),
+    );
+  }
+}
+
+class RomanceDetailPage extends StatefulWidget {
+  @override
+  State<RomanceDetailPage> createState() => _RomanceDetailPageState();
+}
+
+class _RomanceDetailPageState extends State<RomanceDetailPage> {
+  @override
+  _RomanceDetailPageState createState() => _RomanceDetailPageState();
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
+
+class _RomanceDetailPage  extends State<RomanceDetailPage> {
+  final List<String> romanceMovies = [
+    'R1.jpg',
+    'R2.jpg',
+    'R3.jpg',
+    'R4.jpg',
+    'R5.jpg',
+    'R6.jpg',
+    'R7.jpg',
+    'R8.jpg',
+    'R9.jpg',
+  ];
+
+  final List<String> movieNames = [
+    'Titanic',
+    'Raanjhanaa',
+    'Sanam Re',
+    'Love Again',
+    'A Very Venice Romance',
+    'All My Life',
+    'Ae Dil He Mushkil',
+    'Brahmastra',
+    'Dilwale Dulhania Le Jayenge',
+  ];
+
+  List<bool> isLiked = List.generate(9, (index) => false);
+
+  // Dummy ratings for demonstration
+  final List<double> ratings = [9.1, 8, 9, 7.9, 8.2, 9, 6.9, 8, 9.9, 9.1];
+
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Romance Detail"),
+        backgroundColor: Colors.black,
+        title: Text(
+          "Romance Detail",
+          style: TextStyle(
+            color: Colors.yellow,
+            fontWeight: FontWeight.bold,
+            fontSize: 23,
+          ),
       ),
-      body: Container(
-        child: Center(
-          child: Text("Details for Romance Genre"),
+    ),
+     body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Featured',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.yellow,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: romanceMovies.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        print('Tapped on ${movieNames[index]}');
+                      },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.asset(
+                          'images/${romanceMovies[index]}',
+                          fit: BoxFit.cover,
+                          width: 150,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Popular',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.yellow,
+                ),
+              ),
+            ),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.7,
+              ),
+              itemCount: romanceMovies.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    color: Colors.black87,
+                    elevation: 4.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Stack(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(8.0),
+                                topRight: Radius.circular(8.0),
+                              ),
+                              child: Image.asset(
+                                'images/${romanceMovies[index]}',
+                                fit: BoxFit.cover,
+                                height: 180.0, // Increased image height
+                                width: double.infinity,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    movieNames[index],
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                  SizedBox(height: 20),
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        Icons.star,
+                                        color: Colors.yellow,
+                                        size: 25,
+                                      ),
+                                      SizedBox(width: 10),
+                                      Text(
+                                        '${ratings[index]}', // Display movie rating
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 20),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.favorite,
+                                color:
+                                    isLiked[index] ? Colors.red : Colors.grey,
+                              ),
+                              onPressed: () {
+                                // Handle like button press
+                                setState(() {
+                                  isLiked[index] = !isLiked[index];
+                                });
+                                print('Like button pressed');
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
